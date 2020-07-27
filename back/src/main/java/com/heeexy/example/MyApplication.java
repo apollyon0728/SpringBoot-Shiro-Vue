bd2e1a54ed93dcc9139380e23083586b51ac562d
zhengdan10 <93040673@qq.com>
2020-07-27 22:15:57 +0800

    [feat] 尝试增加shardingsphere读写分离

--- back/src/main/java/com/heeexy/example/MyApplication.java
+++ back/src/main/java/com/heeexy/example/MyApplication.java
@@ -3,16 +3,30 @@ package com.heeexy.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+
+/*
+Spring Boot-Shiro-Vue
+提供一套基于SpringBoot-shiro-vue的权限管理思路.
+
+前后端都加以控制,做到按钮/接口级别的权限
+https://github.com/Heeexy/SpringBoot-Shiro-Vue
+ */
 
 /**
  * @author: hxy
  * @description: SpringBoot启动类
  * @date: 2017/10/24 11:55
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableConfigurationProperties
 @MapperScan("com.heeexy.example.dao")
 public class MyApplication extends SpringBootServletInitializer {
 
